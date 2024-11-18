622c988e0c539c0c81acfb96b72050062e99878e
keneni45 <kenotoneaga@gmail.com>
2024-11-18 11:34:45 -0800

    change database

--- birhumindapp/models.py
+++ birhumindapp/models.py
@@ -121,6 +121,7 @@ class DocumentSubmission(models.Model):
     )
     document_type = models.CharField(
         max_length=20,
+       
         choices=[
             ('bds', 'Bds Document'),
             ('business', 'Business'),
